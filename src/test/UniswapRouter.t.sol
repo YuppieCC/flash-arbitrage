f329859daee131e2d6f4a0d4d201f52055b45988
eacent <eacentcheung@gmail.com>
2022-04-22 15:19:37 +0800

    add UniswapRouter.t.sol

---
+++ src/test/UniswapRouter.t.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.10;
+
+import "ds-test/test.sol";
+import {IUniswapV2Router01} from "../interfaces/IUniswapV2Router01.sol";
+
+contract UniswapRouterTest is DSTest {
+    function setUp() public {}
+
+    function testExample() public {
+        assertTrue(true);
+    }
+}
