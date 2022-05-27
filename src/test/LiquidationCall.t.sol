5e77883831f91faeb46e4e9d700750c097489d86
eacent <eacentcheung@gmail.com>
2022-05-27 18:25:27 +0800

    update testRepay pass

--- src/test/LiquidationCall.t.sol
+++ src/test/LiquidationCall.t.sol
@@ -14,8 +14,11 @@ contract LiquidationCallTest is DSTest {
     CheatCodes cheats = CheatCodes(HEVM_ADDRESS);
 
     address public sam = 0x6F82E3cc2a3d6b7A6d98e7941BCadd7f52919D53;
+    address public eacentcheung = 0x6715FcefaaC138C5F2d478973eD13E327dcc2bFa;
     address public AavePool = 0x794a61358D6845594F94dc1DB02A252b5b4814aD;
     address public USDC = 0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174;  // USDC
+    address public WETH = 0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619;  // WETH
+    address public DAI = 0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063;  // DAI
 
     function setUp() public {
         pool = IPool(AavePool);
@@ -96,30 +99,44 @@ contract LiquidationCallTest is DSTest {
     }
 
     function testRepay() public {
-        address asset = USDC;
-        uint256 amount = 1e5;
+        address asset = DAI;
+        uint256 amount = 1e17;
         uint256 interestRateMode = 2;
         address onBehalfOf = sam;
         uint16 referralCode = 0;
         
-        uint beforeBalance = IERC20(USDC).balanceOf(address(sam));
-        console.log("beforeBalance", beforeBalance);
+        cheats.prank(sam);
+        IERC20(asset).approve(address(pool), type(uint).max);
 
         cheats.prank(sam);
-        pool.borrow(asset, amount, interestRateMode, referralCode, onBehalfOf);
-
-        uint afterBalance = IERC20(USDC).balanceOf(address(sam));
-        console.log("afterBalance", afterBalance);
-
-        // cheats.prank(sam);
-        // IERC20(asset).approve(address(pool), amount);
-
-        cheats.prank(sam);
-        uint res = pool.repay(asset, amount, interestRateMode, onBehalfOf);
+        uint res = pool.repay(asset, type(uint).max, interestRateMode, onBehalfOf);
         console.log("res", res);
 
-        uint afterRepayBalance = IERC20(USDC).balanceOf(address(sam));
+        uint afterRepayBalance = IERC20(asset).balanceOf(address(sam));
         console.log("afterRepayBalance", afterRepayBalance);
 
     }
+
+    // function testLiquidationCall() public {
+    //     address collateralAsset = USDC;
+    //     address debtAsset = DAI;
+    //     address user = sam;
+    //     uint256 debtToCover = 5e17;
+    //     bool receiveAToken = false;
+
+    //     address asset = DAI;
+    //     uint256 amount = 10e18;
+    //     uint256 interestRateMode = 2;
+    //     address onBehalfOf = sam;
+    //     uint16 referralCode = 0;
+        
+        // cheats.prank(sam);
+        // pool.borrow(asset, amount, interestRateMode, referralCode, onBehalfOf);
+        // console.log("afterBalance", IERC20(DAI).balanceOf(sam));
+
+    //     console.log("beforeBalance", IERC20(DAI).balanceOf(eacentcheung));
+    //     cheats.prank(eacentcheung);
+    //     pool.liquidationCall(collateralAsset, debtAsset, user, debtToCover, receiveAToken);
+    //     console.log("afterBalance", IERC20(DAI).balanceOf(eacentcheung));
+    // }
 }
