f2c36425dda731db428d3a14a8a77249c175f89f
eacent <eacentcheung@gmail.com>
2022-06-01 10:49:52 +0800

    add script

---
+++ script/Script.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import 'src/Contract.sol';
+import 'forge-std/Script.sol';
+
+
+contract MyScript is Script {
+    function run() external {
+        vm.startBroadcast();
+
+        Contract c = new Contract();
+        c.test();
+        require(c.x() == 12345);
+    }
+}
